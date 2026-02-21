c8f34acef007e730aebf85fef293f88f755a1f3f
Rachit Chhetri <126384125+Racihtchhetri@users.noreply.github.com>
2026-02-21 14:19:28 +0530

    upload.js code correction

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,6 +1,18 @@
 const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+
+const uploadPath = path.join(__dirname, "..", "uploads");
+
+// make sure folder exists
+if (!fs.existsSync(uploadPath)) {
+    fs.mkdirSync(uploadPath, { recursive: true });
+}
+
 const storage = multer.diskStorage({
-    destination: "uploads",
+    destination: (req, file, cb) => {
+        cb(null, uploadPath);
+    },
     filename: (req, file, cb) => {
         cb(null, Date.now() + "-" + file.originalname);
     }
